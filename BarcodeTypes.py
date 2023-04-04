1986c9916a310069c9b1209c258754ed520a08bf
Buddhi Wijenayake <wijenayakebuddhi34802@gmail.com>
2023-04-04 09:13:30 +0530

    Added Barcode Generator

---
+++ BarcodeTypes.py
@@ -0,0 +1,10 @@
+import barcode
+
+barcode_formats = barcode.PROVIDED_BARCODES
+
+print(barcode_formats)
+
+
+# ['codabar', 'code128', 'code39', 'ean', 'ean13', 'ean13-guard', 'ean14', 'ean8',
+#  'ean8-guard', 'gs1', 'gs1_128', 'gtin', 'isbn', 'isbn10', 'isbn13', 'issn', 'itf',
+#  'jan', 'nw-7', 'pzn', 'upc', 'upca']

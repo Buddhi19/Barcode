fd693fd55f5bd4c652759ff5b47796c2c5c24b81
Buddhi Wijenayake <wijenayakebuddhi34802@gmail.com>
2023-04-30 01:12:15 +0530

    modified

--- GenerateBarCodes/generateBarCode.py
+++ GenerateBarCodes/generateBarCode.py
@@ -1,14 +1,37 @@
 import barcode
 from barcode.writer import ImageWriter
   
+
+def generate_types():
+    barcode_formats = barcode.PROVIDED_BARCODES
+    for num in barcode_formats:
+        print(num,end=", ")
+    print("\n")
 #Define content of the barcode as a string
-number = 'helloworld-1234'
+def generate_barcode(text,type):
+    number = text
 
-#Get the required barcode format
-barcode_format = barcode.get_barcode_class('code39')
+    #Get the required barcode format
+    barcode_format = barcode.get_barcode_class(type)
 
-#Generate barcode and render as image
-my_barcode = barcode_format(number, writer=ImageWriter())
-  
-#Save barcode as PNG
-my_barcode.save("BarCode")
+    #Generate barcode and render as image
+    my_barcode = barcode_format(number, writer=ImageWriter())
+    
+    #Save barcode as PNG
+    my_barcode.save(f"BarCode- {text}")
+
+if __name__=="__main__":
+    print("Choose a Barcode Type")
+    generate_types()
+    type=input()
+    while True:
+        try:
+            text=input()
+            if text:
+                generate_barcode(text,type)
+            else:
+                break
+            
+
+        except:
+            break
